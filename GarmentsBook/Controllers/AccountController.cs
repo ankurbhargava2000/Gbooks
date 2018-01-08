84347a6e37a4665823edf829838137ff323ad70c
Ankur <Ankur@LAPTOP-K0DFQCFE>
2018-01-08 09:09:31 +0530

    LogOff

--- GarmentsBook/Controllers/AccountController.cs
+++ GarmentsBook/Controllers/AccountController.cs
@@ -427,7 +427,7 @@ namespace GarmentSoft.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Login", "Account");
         }
 
         //
